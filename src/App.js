7311d99886c5708aa3830d4b3cb53bbd512d2baf
Kamana04 <kamana.mathur89@gmail.com>
2021-09-08 22:26:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import {incNumber,decNumber,multNumber,divNumber} from './actions/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const multState = useSelector((state) => state.multTheNumber);
+  const dispatch = useDispatch();
+  return(
+    <>
+         <div className="container">
+           <h1>Increment/Decrement counter</h1>
+           <h4>using React and Redux</h4>
+
+           <div className="quantity">
+             <a className="quantity__minus" title="Decrement"
+                       onClick={() => dispatch(decNumber())}><span>-</span></a>
+             <input name="quantity" type="text" className="quantity__input" value={myState} />
+             <a className="quantity__plus" title="Increment"
+                    onClick={() => dispatch(incNumber(5))}><span>+</span></a>
+           </div>
+         </div>
+
+         <div className="container my-5">
+           <h1>Multiplication/Divide counter</h1>
+           <h4>using React and Redux</h4>
+
+           <div className="quantity">
+             <a className="quantity__minus" title="Decrement"
+                       onClick={() => dispatch(divNumber())}><span>/</span></a>
+             <input name="quantity" type="text" className="quantity__input" value={multState} />
+             <a className="quantity__plus" title="Increment"
+                    onClick={() => dispatch(multNumber(5))}><span>*</span></a>
+           </div>
+         </div>
+    </>
+  )
 }
-
 export default App;
